4aed4311ec308261ef7466676b225a4d175e5918
OnedayLiu <onedayliu552@gmail.com>
2018-01-26 11:41:39 +0800

    feat: 增加用户名密码字段的自定义配置

--- config/config.default.js
+++ config/config.default.js
@@ -2,4 +2,6 @@
 // /lib/plugin/egg-passport-local/config
 // 配置可参考 https://github.com/jaredhanson/passport-local
 exports.passportLocal = {
+  usernameField: 'username',
+  passwordField: 'password',
 };
